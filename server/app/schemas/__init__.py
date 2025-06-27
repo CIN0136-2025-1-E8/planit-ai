b115a7703c1d612aacbc1b7a7fb4ca28577bbcac
Denilson Silva <43222321+altinctrl@users.noreply.github.com>
2025-06-27 08:56:38 -0300

    feat: Implement evaluation schema

--- server/app/schemas/__init__.py
+++ server/app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .basic_chat import BasicChatHistory, BasicChatRequest, BasicChatResponse, BasicChatHistoryRequest
 from .chat_schema import ChatRoles, ChatPartTypes, ChatPart, ChatMessageParts, ChatResponse, ChatHistory, ChatsList
 from .course_schema import Course, CourseBase, CourseCreate, CourseUpdate, CourseSummary
-from .evaluation_schema import Evaluation, EvaluationBase, EvaluationCreate, EvaluationUpdate
+from .evaluation_schema import EvaluationTypes, Evaluation, EvaluationBase, EvaluationCreate, EvaluationUpdate
 from .file_schema import FileRecord
 from .lecture_schema import Lecture, LectureBase, LectureCreate, LectureUpdate
--- server/app/schemas/evaluation_schema.py
+++ server/app/schemas/evaluation_schema.py
@@ -1,8 +1,22 @@
+from enum import Enum
+
 from pydantic import BaseModel, ConfigDict
 
 
+class EvaluationTypes(Enum):
+    EXAM = "exam"
+    QUIZ = "quiz"
+    ASSIGNMENT = "assignment"  # Generic term for the submission of written assignments, problem sets, projects, etc.
+    PRESENTATION = "presentation"
+    LAB = "lab"
+
+
 class EvaluationBase(BaseModel):
-    pass
+    type: str
+    title: str
+    start_datetime: str
+    end_datetime: str
+    present: bool | None = None
 
 
 class EvaluationCreate(EvaluationBase):
@@ -10,7 +24,11 @@ class EvaluationCreate(EvaluationBase):
 
 
 class EvaluationUpdate(BaseModel):
-    pass
+    type: str | None
+    title: str | None
+    start_datetime: str | None
+    end_datetime: str | None
+    present: bool | None = None
 
 
 class Evaluation(EvaluationBase):
