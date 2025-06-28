5bf3a154be7d1d25f2523409ae57b14ff2f0481f
Denilson Silva <43222321+altinctrl@users.noreply.github.com>
2025-06-28 13:59:31 -0300

    feat: Implement base crud
    (cherry picked from commit 84cc04a5ee1a9b21378c1e2c8e1eaabe0e47bf98)
    (cherry picked from commit 012bd9105171325dec05b52d42360680e356de82)

--- server/app/crud/__init__.py
+++ server/app/crud/__init__.py
@@ -1,3 +1,4 @@
+from .base import CRUDBase
 from .chat_crud import chat_crud, get_chat_crud, CRUDChat
 from .course_crud import course_crud, get_course_crud, CRUDCourse
 from .files_crud import files_crud, get_files_crud, CRUDFiles
