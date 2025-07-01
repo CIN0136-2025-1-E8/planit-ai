b8f65006ee8dca6a6a0dec4225e8fd37e405de85
Denilson Silva <43222321+altinctrl@users.noreply.github.com>
2025-06-30 23:26:24 -0300

    feat: Implement google_ai_service
    (cherry picked from commit 7e644ce9cd4ea79da26f5e0847a7433833e57804)

---
+++ server/app/services/google_ai_service.py
@@ -0,0 +1,26 @@
+from google import genai
+from google.genai.types import Content
+
+from core import settings
+
+
+def get_google_ai_service():
+    return google_ai_service
+
+
+class GoogleAIService:
+    def __init__(self, api_key: str):
+        if not api_key:
+            raise ValueError("Google API key not configured.")
+        self.client = genai.Client(api_key=api_key)
+
+    async def send_message(self,
+                           message: str,
+                           chat_history: list[Content] | None = None
+                           ) -> tuple[str, list[Content]]:
+        chat = self.client.aio.chats.create(model=settings.GOOGLE_BASIC_MODEL, history=chat_history)
+        response = await chat.send_message(message)
+        return response.text, chat.get_history()
+
+
+google_ai_service = GoogleAIService(api_key=settings.GOOGLE_API_KEY.get_secret_value())
