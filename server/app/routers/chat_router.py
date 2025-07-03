0e484004fa492c579565848f37ba0b2b35ea2251
Denilson Silva <43222321+altinctrl@users.noreply.github.com>
2025-07-02 22:11:56 -0300

    feat: Add system instruction to the model configuration used in the chat and send the current datetime with every message to improve response accuracy
    (cherry picked from commit e2d2cd596d4f526cff450c688fb875c731de24b8)

--- server/app/routers/chat_router.py
+++ server/app/routers/chat_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends, Form
 
+from core import settings
 from crud import get_chat_crud
 from schemas import ChatMessage, ChatRole
 from services import get_google_ai_service
+from utils import system_message_current_datetime
 
 chat_router = APIRouter(
     prefix="/chat",
@@ -21,7 +23,9 @@ async def send_chat_message(message: str = Form(),
                             crud=Depends(get_chat_crud),
                             ai_service=Depends(get_google_ai_service)):
     llm_context = crud.get_llm_context()
-    response, new_content = await ai_service.send_message(message=message, llm_context=llm_context)
+    response, new_content = await ai_service.send_message(instruction=settings.CHAT_SYSTEM_INSTRUCTIONS,
+                                                          message="".join([system_message_current_datetime(), message]),
+                                                          llm_context=llm_context)
     crud.append_llm_context(new_content)
     crud.append_chat_history(ChatMessage(role=ChatRole.USER, text=message))
     crud.append_chat_history(ChatMessage(role=ChatRole.MODEL, text=response))
