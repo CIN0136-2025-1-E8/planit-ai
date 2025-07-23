8636f2f128f8c649af17b503a3189f8049a9fa3e
tam6 <tam6@g2c27.cin.ufpe.br>
2025-07-23 13:01:15 -0300

    atualizando o chat ṕara entrar na base de dados

--- server/app/routers/chat_router.py
+++ server/app/routers/chat_router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends, Form
+from sqlalchemy.orm import Session
 
 from app.core import settings
+from app.core.security import get_current_user
 from app.crud import get_chat_crud
+from app.dependencies import get_db
+from app.models import User
 from app.schemas import ChatMessage, ChatRole
 from app.services import get_google_ai_service
 
@@ -11,16 +15,22 @@ chat_router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-
 @chat_router.get("/history", response_model=list[ChatMessage])
-async def get_chat_history(crud=Depends(get_chat_crud)):
-    return crud.get_chat_history()
-
+async def get_chat_history(
+    crud=Depends(get_chat_crud),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    return crud.get_chat_history(db=db, user=current_user)
 
 @chat_router.post("/message", response_model=str)
-async def send_chat_message(message: str = Form(),
-                            crud=Depends(get_chat_crud),
-                            ai_service=Depends(get_google_ai_service)):
+async def send_chat_message(
+    message: str = Form(),
+    crud=Depends(get_chat_crud),
+    ai_service=Depends(get_google_ai_service),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     llm_context = crud.get_llm_context()
     try:
         response, new_content = await ai_service.send_message(
@@ -29,7 +39,13 @@ async def send_chat_message(message: str = Form(),
             llm_context=llm_context)
     except Exception:
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
     crud.append_llm_context(new_content)
-    crud.append_chat_history(ChatMessage(role=ChatRole.USER, text=message))
-    crud.append_chat_history(ChatMessage(role=ChatRole.MODEL, text=response))
-    return response
+    
+    user_message = ChatMessage(role=ChatRole.USER, text=message)
+    model_message = ChatMessage(role=ChatRole.MODEL, text=response)
+    
+    crud.append_chat_history(db=db, user=current_user, chat_message=user_message)
+    crud.append_chat_history(db=db, user=current_user, chat_message=model_message)
+    
+    return response #o
