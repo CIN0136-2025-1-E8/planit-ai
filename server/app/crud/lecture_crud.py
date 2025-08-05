c0d6c61e07af93803e6d929e737e55c7d76aefae
Denilson Silva <43222321+altinctrl@users.noreply.github.com>
2025-08-05 19:42:31 -0300

    refactor: Remove redundant code

--- server/app/crud/lecture_crud.py
+++ server/app/crud/lecture_crud.py
@@ -39,21 +39,5 @@ class CRUDLecture(CRUDBase[Lecture, LectureCreate, LectureUpdate]):
         results = list(db.execute(query).scalars().all())
         return results
 
-    def get_all_by_course(self, db: Session, *, course_uuid: str) -> list[Lecture]:
-        query = select(self.model).filter(self.model.course_uuid == course_uuid)
-        result = db.execute(query).scalars().all()
-        return list(result)
-
-    def get_by_date(self, db: Session, *, day: str) -> list[Lecture]:
-        query = select(self.model).filter(self.model.start_datetime.startswith(day)) # analise
-        result = db.execute(query).scalars().all()
-        return list(result)
-
-    def set_presence(self, db: Session, *, uuid: str, present: bool) -> Lecture | None:
-        lecture = self.get(db, obj_uuid=uuid)
-        if lecture:
-            return self.update(db, db_obj=lecture, obj_in={"present": present})
-        return None
-
 
 lecture_crud = CRUDLecture(Lecture)
